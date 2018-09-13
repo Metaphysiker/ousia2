4eb967aedea3da39a1c66d2b09ac8eced8d45d94
Metaphysiker <s.raess@me.com>
2018-09-13 09:38:21 +0200

    added staticpages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'static_pages#welcome'
+  #get '/patients/:id', to: 'patients#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
